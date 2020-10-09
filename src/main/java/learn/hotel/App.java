bccd09e1b9be5200534c89dfd9eac1913a2c701a
Cecilia <cbarkume@dev-10.com>
2020-10-09 11:08:36 -0500

    Started on Controller and added .xml configuration.

--- src/main/java/learn/hotel/App.java
+++ src/main/java/learn/hotel/App.java
@@ -1,7 +1,15 @@
 package learn.hotel;
 
+import learn.hotel.ui.Controller;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
     public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("dependency-configuration.xml");
 
+        Controller controller = context.getBean(Controller.class);
+        // Run the app!
+        //controller.run();
     }
 }
--- src/main/java/learn/hotel/ui/Controller.java
+++ src/main/java/learn/hotel/ui/Controller.java
@@ -1,4 +1,21 @@
 package learn.hotel.ui;
 
+import learn.hotel.domain.GuestService;
+import learn.hotel.domain.HostService;
+import learn.hotel.domain.ReservationService;
+
 public class Controller {
+
+    private final GuestService guestService;
+    private final HostService hostService;
+    private final ReservationService reservationService;
+    private final View view;
+
+    public Controller(GuestService guestService, HostService hostService, ReservationService reservationService, View view) {
+        this.guestService = guestService;
+        this.hostService = hostService;
+        this.reservationService = reservationService;
+        this.view = view;
+    }
+
 }
