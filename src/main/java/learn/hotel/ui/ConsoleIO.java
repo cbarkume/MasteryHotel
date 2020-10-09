f8ce5374522ffa1d69fb507e646dd950106f2937
Cecilia <cbarkume@dev-10.com>
2020-10-09 10:57:39 -0500

    Started on the View class and added display methods to ConsoleIO.

--- src/main/java/learn/hotel/ui/ConsoleIO.java
+++ src/main/java/learn/hotel/ui/ConsoleIO.java
@@ -1,9 +1,12 @@
 package learn.hotel.ui;
 
+import learn.hotel.models.*;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.format.DateTimeParseException;
+import java.util.List;
 import java.util.Scanner;
 
 public class ConsoleIO {
@@ -128,6 +131,90 @@ public class ConsoleIO {
                 return result;
             }
             println(String.format(NUMBER_OUT_OF_RANGE, min, max));
+        }
+    }
+
+    public void enterToContinue() {
+        readString("Press [Enter] to continue.");
+    }
+
+
+    public void displayHeader(String message) {
+        println("");
+        println(message);
+        println("=".repeat(message.length()));
+    }
+
+    public void displayTotal(BigDecimal total) {
+        println("$" + total.toString());
+    }
+
+    public void displayException(Exception ex) {
+        displayHeader("A critical error occurred:");
+        println(ex.getMessage());
+    }
+
+    public void displayStatus(boolean success, String message) {
+        displayStatus(success, List.of(message));
+    }
+
+    public void displayStatus(boolean success, List<String> messages) {
+        displayHeader(success ? "Success" : "Error");
+        for (String message : messages) {
+            println(message);
+        }
+    }
+
+    public void displayGuests(List<Guest> guests) {
+        if (guests == null || guests.isEmpty()) {
+            println("No guests found.");
+            return;
+        }
+        for (Guest guest : guests) {
+            printf("Guest Name: %s %s, Phone: %s, Email: %s, State: %s%n",
+                    guest.getFirstName(),
+                    guest.getLastName(),
+                    guest.getPhone(),
+                    guest.getEmail(),
+                    guest.getState().fullName);
+            );
+        }
+    }
+
+    public void displayHosts(List<Host> hosts) {
+        if (hosts == null || hosts.isEmpty()) {
+            println("No hosts found.");
+            return;
+        }
+        for (Host host : hosts) {
+            printf("Host Name: %s, Phone Number: %s, Email: %s, Address: %s, %s, %s, %s, Standard Rate: $%s, Weekend Rate: $%s%n",
+                    host.getLastName(),
+                    host.getPhone(),
+                    host.getEmail(),
+                    host.getAddress(),
+                    host.getCity(),
+                    host.getState(),
+                    host.getPostalCode(),
+                    host.getStandardRate(),
+                    host.getWeekendRate());
+
+        }
+    }
+
+    public void displayReservations(List<Reservation> reservations) {
+
+        if (reservations.size() == 0) {
+            println("No items found");
+        }
+
+        for (Reservation reservation : reservations) {
+            printf("Host Name: %s, Guest Name: %s %s, Duration: %s to %s, Total: $%s%n",
+                    reservation.getHost().getLastName(),
+                    reservation.getGuest().getFirstName(),
+                    reservation.getGuest().getLastName(),
+                    reservation.getStartDate().toString(),
+                    reservation.getEndDate().toString(),
+                    reservation.getTotal());
         }
     }
 }
--- src/main/java/learn/hotel/ui/View.java
+++ src/main/java/learn/hotel/ui/View.java
@@ -1,4 +1,24 @@
 package learn.hotel.ui;
 
 public class View {
+
+    private final ConsoleIO io;
+
+    public View(ConsoleIO io) {
+        this.io = io;
+    }
+
+    public MainMenuOption selectMainMenuOption() {
+        io.displayHeader("Main Menu");
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for (MainMenuOption option : MainMenuOption.values()) {
+            io.printf("%s. %s%n", option.getValue(), option.getMessage());
+            min = Math.min(min, option.getValue());
+            max = Math.max(max, option.getValue());
+        }
+
+        String message = String.format("Select [%s-%s]: ", min, max);
+        return MainMenuOption.fromValue(io.readInt(message, min, max));
+    }
 }
